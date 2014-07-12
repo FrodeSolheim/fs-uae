02e47c27917302ba4b9cf97e34caaf6f09f87b75
Frode Solheim <frode-code@fengestad.no>
2014-07-12 11:10:17 +0200

    changes to compile with newer XCode / SDK

--- libfsemu/include/fs/glee.h
+++ libfsemu/include/fs/glee.h
@@ -760,12 +760,21 @@ GLEE_EXTERN GLboolean _GLEE_SGIX_texture_range;
 #endif
 
 #ifndef GL_ARB_vertex_buffer_object
+#if defined(__APPLE__)
+    typedef long GLintptrARB;
+    typedef long GLsizeiptrARB;
+#else
     typedef ptrdiff_t GLintptrARB;
     typedef ptrdiff_t GLsizeiptrARB;
 #endif
+#endif
 
 #ifndef GL_ARB_shader_objects
+#if defined(__APPLE__)
+    typedef void *GLhandleARB;
+#else
     typedef int GLhandleARB;
+#endif
     typedef char GLcharARB;
 #endif
 

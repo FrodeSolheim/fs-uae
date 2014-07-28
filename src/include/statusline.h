c61732fe946cdf6ea8f5016d463aca10121325c9
Frode Solheim <frode-code@fengestad.no>
2014-07-28 18:23:23 +0200

    statusline change

--- src/include/statusline.h
+++ src/include/statusline.h
@@ -10,6 +10,12 @@
 #define TD_RIGHT 1
 #define TD_BOTTOM 2
 
+#ifdef FSUAE
+#define td_pos (TD_RIGHT | TD_BOTTOM)
+#else
+static int td_pos = (TD_RIGHT | TD_BOTTOM);
+#endif
+
 #define TD_NUM_WIDTH 7
 #define TD_NUM_HEIGHT 7
 
@@ -24,7 +30,6 @@
 #define STATUSLINE_TARGET 0x80
 
 extern void draw_status_line_single (uae_u8 *buf, int bpp, int y, int totalwidth, uae_u32 *rc, uae_u32 *gc, uae_u32 *bc, uae_u32 *alpha);
-extern void statusline_getpos (int *x, int *y, int width, int height);
 extern void statusline_single_erase(uae_u8 *buf, int bpp, int y, int totalwidth);
 extern void statusline_getpos(int *x, int *y, int width, int height);
 

4fd7a1cf9021a5c5088132d054135847661066c4
Frode Solheim <frode-code@fengestad.no>
2014-07-31 22:47:54 +0200

    Makefile.am fix, updated copyrights

--- src/fs-uae/main.c
+++ src/fs-uae/main.c
@@ -848,9 +848,9 @@ static const char *overlay_names[] = {
 };
 
 #define COPYRIGHT_NOTICE "\nFS-UAE VERSION %s\n" \
-"Copyright 1995-2002 Bernd Schmidt, 1999-2012 Toni Wilen,\n" \
+"Copyright 1995-2002 Bernd Schmidt, 1999-2014 Toni Wilen,\n" \
 "2003-2007 Richard Drummond, 2006-2011 Mustafa 'GnoStiC' Tufan,\n" \
-"2011-2013 Frode Solheim, and contributors.\n" \
+"2011-2014 Frode Solheim, and contributors.\n" \
 "\n" \
 "This is free software; see the file COPYING for copying conditions. There\n" \
 "is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR\n" \

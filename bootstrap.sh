6010e6d45c601e59aab92bf74d38f8c7a05a2cb3
Frode Solheim <frode@fs-uae.net>
2014-08-09 10:28:08 +0200

    misc autotool fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,12 @@ echo "Boostrapping fs-uae..."
 # version number is cached in autom4te.cache
 rm -Rf autom4te.cache
 # cat m4/* | grep -v "#serial" > acinclude.m4
-echo "Running aclocal -I m4"
+echo "Running aclocal"
 if [ -e /opt/share/aclocal ]; then
 	# extra m4 macros installed on mingw system
-	aclocal -I m4 -I /opt/share/aclocal
+	aclocal -I /opt/share/aclocal
 else
-	aclocal -I m4
+	aclocal
 fi
 echo "Running autoheader"
 autoheader

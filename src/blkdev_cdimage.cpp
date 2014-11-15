400200b67cf554b3a72d3edf41cc269a650bd86a
Frode Solheim <frode@fs-uae.net>
2014-11-15 12:45:22 +0100

    2336->2048 sector conversion fix [WinUAE 479073e1]

--- src/blkdev_cdimage.cpp
+++ src/blkdev_cdimage.cpp
@@ -933,7 +933,7 @@ end:
 	return ret;
 }
 
-// this only supports 2048 byte sectors
+// return standard 2048 byte sectors only
 static int command_read (int unitnum, uae_u8 *data, int sector, int numsectors)
 {
 	struct cdunit *cdu = unitisopen (unitnum);
@@ -957,7 +957,8 @@ static int command_read (int unitnum, uae_u8 *data, int sector, int numsectors)
 				// 2 = MODE2
 				do_read (cdu, t, data, sector, b == 2 ? 24 : 16, 2048, false);
 			} else {
-				do_read (cdu, t, data, sector, 16, 2048, false);
+				// 2336
+				do_read (cdu, t, data, sector, 8, 2048, false);
 			}
 			data += 2048;
 			sector++;

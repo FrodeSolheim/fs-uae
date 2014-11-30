a91a8afdbe136c0ae3387455a6a3d74ef02ca513
Frode Solheim <frode@fs-uae.net>
2014-11-30 12:40:06 +0100

    uaenet.device slirp support fix [WinUAE 80bb5a41]

--- src/include/ethernet.h
+++ src/include/ethernet.h
@@ -21,8 +21,8 @@ struct netdriverdata
 };
 
 
-typedef void (ethernet_gotfunc)(struct s2devstruct *dev, const uae_u8 *data, int len);
-typedef int (ethernet_getfunc)(struct s2devstruct *dev, uae_u8 *d, int *len);
+typedef void (ethernet_gotfunc)(void *dev, const uae_u8 *data, int len);
+typedef int (ethernet_getfunc)(void *dev, uae_u8 *d, int *len);
 
 extern bool ethernet_enumerate (struct netdriverdata **, const TCHAR *name);
 extern void ethernet_enumerate_free (void);
@@ -32,6 +32,6 @@ extern int ethernet_getdatalenght (struct netdriverdata *ndd);
 extern int ethernet_getbytespending (void*);
 extern int ethernet_open (struct netdriverdata *ndd, void*, void*, ethernet_gotfunc*, ethernet_getfunc*, int);
 extern void ethernet_close (struct netdriverdata *ndd, void*);
-extern void ethernet_trigger (void*);
+extern void ethernet_trigger (struct netdriverdata *ndd, void*);
 
 #endif // UAE_ETHERNET_H

59bef93e8320adc9a7237654130a25789dbc9111
Frode Solheim <frode@fs-uae.net>
2014-12-27 14:13:37 +0100

    read plugins from [prefix]/lib/fs-uae/plugins too

--- src/fs-uae/plugins.c
+++ src/fs-uae/plugins.c
@@ -47,34 +47,51 @@ static const char *lookup_plugin(const char *name)
         // FIXME: resource leak, should cache the path
         return (const char*) path;
     }
-    g_free(module_name);
     g_free(path);
 
-    void *data = g_hash_table_lookup(provides, name);
+    void *data = g_hash_table_lookup(provides, module_name);
+    g_free(module_name);
+
+    if (!data) {
+        data = g_hash_table_lookup(provides, name);
+    }
     return (const char *) data;
 }
 
 static void load_plugin_provides(const char *path, GKeyFile *key_file,
-                                 const char *group_name)
+                                 const char *group_name, int scan)
 {
     gchar **keys = g_key_file_get_keys(key_file, group_name, NULL, NULL);
-    if (keys == NULL) {
-        return;
+    if (keys) {
+        for (gchar *key = *keys; *key; key++) {
+            gchar *value = g_key_file_get_string(key_file, group_name,
+                                                 key, NULL);
+            if (value == NULL) {
+                continue;
+            }
+            gchar *p = g_build_filename(path, value, NULL);
+            if (g_file_test(p, G_FILE_TEST_EXISTS)) {
+                g_hash_table_insert(provides, g_strdup(key), p);
+            }
+            else {
+                g_free(p);
+            }
+        }
+        g_strfreev(keys);
     }
-    for (gchar *key = *keys; *key; key++) {
-        gchar *value = g_key_file_get_string(key_file, group_name, key, NULL);
-        if (value == NULL) {
-            continue;
-        }
-        gchar *p = g_build_filename(path, value, NULL);
-        if (g_file_test(p, G_FILE_TEST_EXISTS)) {
-            g_hash_table_insert(provides, g_strdup(key), p);
-        }
-        else {
-            g_free(p);
+    if (scan) {
+        GDir *dir = g_dir_open(path, 0, NULL);
+        if (dir) {
+            const gchar *name;
+            while ((name = g_dir_read_name(dir))) {
+                gchar *p = g_build_filename(path, name, NULL);
+                g_hash_table_insert(provides, g_strdup(name), p);
+            }
+            g_dir_close(dir);
+        } else {
+            fs_log("Warning: could not open plugin dir %s\n", path);
         }
     }
-    g_strfreev(keys);
 }
 
 static void load_plugin(const char *path, const char *ini_path)
@@ -92,11 +109,57 @@ static void load_plugin(const char *path, const char *ini_path)
     }
     gchar *version_path = g_build_filename(path, version, NULL);
     fs_log("-> %s\n", version_path);
-    load_plugin_provides(version_path, key_file, "provides");
-    load_plugin_provides(version_path, key_file, OS_NAME "-" ARCH_NAME);
+    load_plugin_provides(version_path, key_file, "provides", 0);
+
+    // FIXME: deprecated
+    load_plugin_provides(version_path, key_file, OS_NAME "-" ARCH_NAME, 1);
+
+#ifdef __i386__
+    load_plugin_provides(version_path, key_file, OS_NAME "_i386", 1);
+#endif
+#ifdef __i686__
+    /* If there's an i686 version of the plugin, we want to use that instead
+     * of the i386 one. */
+    load_plugin_provides(version_path, key_file, OS_NAME "_i686", 1);
+#ifdef STEAMOS
+    load_plugin_provides(version_path, key_file, "steamos_i686", 1);
+#endif
+#endif
+#ifdef __x86_64__
+    load_plugin_provides(version_path, key_file, OS_NAME "_x86-64", 1);
+#ifdef STEAMOS
+    load_plugin_provides(version_path, key_file, "steamos_x86-64", 1);
+#endif
+#endif
+#ifdef __ppc__
+    load_plugin_provides(version_path, key_file, OS_NAME "_ppc", 1);
+#endif
+
     g_free(version_path);
     g_free(version);
     g_key_file_free(key_file);
+}
+
+static void load_plugins_from_dir(const char *plugins_dir)
+{
+    fs_log("loading plugins from %s\n", plugins_dir);
+    GDir *dir = g_dir_open(plugins_dir, 0, NULL);
+    if (!dir) {
+        fs_log("Warning: could not open plugins dir %s\n", plugins_dir);
+        return;
+    }
+    const gchar *name;
+    while ((name = g_dir_read_name(dir))) {
+        gchar *p = g_build_filename(plugins_dir, name, NULL);
+        gchar *p2 = g_build_filename(p, "plugin.ini", NULL);
+        fs_log("Checking %s\n", p2);
+        if (g_file_test(p2, G_FILE_TEST_IS_REGULAR)) {
+            load_plugin(p, p2);
+        }
+        g_free(p2);
+        g_free(p);
+    }
+    g_dir_close(dir);
 }
 
 void fs_uae_plugins_init()
@@ -106,21 +169,14 @@ void fs_uae_plugins_init()
     amiga_set_plugin_lookup_function(lookup_plugin);
 
     const char *plugins_dir = fs_uae_plugins_dir();
-    GDir *dir = g_dir_open(plugins_dir, 0, NULL);
-    if (!dir) {
-        fs_log("Warning: could not open plugins dir\n");
-        return;
+    load_plugins_from_dir(plugins_dir);
+
+    char executable_dir[FS_PATH_MAX];
+    fs_get_application_exe_dir(executable_dir, FS_PATH_MAX);
+    gchar *path = g_build_filename(executable_dir, "..", "lib",
+                                   "fs-uae", "plugins", NULL);
+    if (g_file_test(path, G_FILE_TEST_IS_DIR)) {
+        load_plugins_from_dir(path);
     }
-    const gchar *name;
-    while ((name = g_dir_read_name(dir))) {
-        char *p = g_build_filename(plugins_dir, name, NULL);
-        char *p2 = g_build_filename(p, "plugin.ini", NULL);
-        fs_log("Checking %s\n", p2);
-        if (g_file_test(p2, G_FILE_TEST_IS_REGULAR)) {
-            load_plugin(p, p2);
-        }
-        g_free(p2);
-        g_free(p);
-    }
-    g_dir_close(dir);
+    g_free(path);
 }

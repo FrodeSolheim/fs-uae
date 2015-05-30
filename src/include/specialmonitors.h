445026d7da5f5d5a55ffb97c950680ef8b7ee5c3
Frode Solheim <frode@fs-uae.net>
2015-05-30 22:47:47 +0200

    FireCracker 24 [WinUAE f8bea029]

--- src/include/specialmonitors.h
+++ src/include/specialmonitors.h
@@ -5,5 +5,8 @@
 
 bool emulate_specialmonitors(struct vidbuffer *src, struct vidbuffer *dst);
 void specialmonitor_store_fmode(int vpos, int hpos, uae_u16 fmode);
+void specialmonitor_reset(void);
+bool specialmonitor_need_genlock(void);
+addrbank *specialmonitor_autoconfig_init(int devnum);
 
 #endif /* UAE_SPECIALMONITORS_H */

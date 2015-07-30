cba9175a54d5889f51038cb9f6759f4019049d88
Frode Solheim <frode@fs-uae.net>
2015-07-30 17:44:22 +0200

    Genlock transparency emulation [WinUAE 4d65ec3b]

--- src/include/specialmonitors.h
+++ src/include/specialmonitors.h
@@ -9,5 +9,6 @@ void specialmonitor_store_fmode(int vpos, int hpos, uae_u16 fmode);
 void specialmonitor_reset(void);
 bool specialmonitor_need_genlock(void);
 addrbank *specialmonitor_autoconfig_init(int devnum);
+bool emulate_genlock(struct vidbuffer*, struct vidbuffer*);
 
 #endif /* UAE_SPECIALMONITORS_H */

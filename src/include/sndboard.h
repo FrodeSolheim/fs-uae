0327982c5c7060567a966b2993e983685c0bc8a0
Frode Solheim <frode@fs-uae.net>
2016-12-29 17:53:56 +0100

    Summer 2016 big update part 1. [WinUAE 3f9da39e]

--- src/include/sndboard.h
+++ src/include/sndboard.h
@@ -6,7 +6,7 @@
 #include "uae/memory.h"
 #endif
 
-addrbank *sndboard_init(int devnum);
+bool sndboard_init(struct autoconfig_info *aci);
 void sndboard_free(void);
 void sndboard_hsync(void);
 void sndboard_vsync(void);
@@ -14,5 +14,11 @@ void sndboard_rethink(void);
 void update_sndboard_sound(double);
 void sndboard_reset(void);
 void sndboard_ext_volume(void);
+
+bool uaesndboard_init_z2(struct autoconfig_info *aci);
+bool uaesndboard_init_z3(struct autoconfig_info *aci);
+void uaesndboard_free(void);
+void uaesndboard_reset(void);
+
 
 #endif /* UAE_SNDBOARD_H */

336f8a853d13658df66664e2add5324558b39f82
Frode Solheim <frode@solheim.dev>
2019-05-08 23:08:30 +0200

    Fixes for 3610b4

--- src/od-fs/threading.cpp
+++ src/od-fs/threading.cpp
@@ -1,5 +1,6 @@
 #include "sysconfig.h"
 #include "sysdeps.h"
+#include "uae.h"
 #include <fs/thread.h>
 
 int uae_start_thread_fast (void *(*f)(void *), void *arg,
@@ -62,6 +63,11 @@ bool uae_is_main_thread(void)
     return SDL_ThreadID() == g_main_thread_id;
 }
 
+bool is_mainthread(void)
+{
+    return uae_is_main_thread();
+}
+
 void uae_register_emulation_thread(void)
 {
     g_emulation_thread_id = SDL_ThreadID();
@@ -74,24 +80,28 @@ bool uae_is_emulation_thread(void)
 
 /* FIXME: Move atomic functions to header for inlining? */
 
-void atomic_and(volatile uae_atomic *p, uae_u32 v)
+uae_atomic atomic_and(volatile uae_atomic *p, uae_u32 v)
 {
-    __sync_fetch_and_and(p, v);
+    // FIXME: signature has changed, check return value!
+    return __sync_fetch_and_and(p, v);
 }
 
-void atomic_or(volatile uae_atomic *p, uae_u32 v)
+uae_atomic atomic_or(volatile uae_atomic *p, uae_u32 v)
 {
-    __sync_fetch_and_or(p, v);
+    // FIXME: signature has changed, check return value!
+    return __sync_fetch_and_or(p, v);
 }
 
 uae_atomic atomic_inc(volatile uae_atomic *p)
 {
-    __sync_fetch_and_add(p, 1);
+    // FIXME: signature has changed, check return value!
+    return __sync_fetch_and_add(p, 1);
 }
 
 uae_atomic atomic_dec(volatile uae_atomic *p)
 {
-    __sync_fetch_and_sub(p, 1);
+    // FIXME: signature has changed, check return value!
+    return __sync_fetch_and_sub(p, 1);
 }
 
 uae_u32 atomic_bit_test_and_reset(volatile uae_atomic *p, uae_u32 v)

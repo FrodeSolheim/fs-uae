688fa25e0ed2f4d19aacb7d2d275b51aad913d2a
Frode Solheim <frode@solheim.dev>
2019-05-09 22:06:01 +0200

    Debugger updates, catch application's unhandled exception [WinUAE 84f78c73]

--- src/newcpu.cpp
+++ src/newcpu.cpp
@@ -3657,6 +3657,9 @@ static void ExceptionX (int nr, uaecptr address)
 	if (cpu_tracer) {
 		cputrace.state = nr;
 	}
+	if (!regs.s) {
+		regs.instruction_pc_user_exception = m68k_getpc();
+	}
 
 #ifdef JIT
 	if (currprefs.cachesize)

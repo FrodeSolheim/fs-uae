c6efc998276d9e00a02d0a7e514f0d64fe4af728
Frode Solheim <frode@solheim.dev>
2021-02-23 19:10:53 +0100

    More updates to build files

--- fsbuild/common.inc.sh
+++ fsbuild/common.inc.sh
@@ -3,11 +3,14 @@ PACKAGE=`cat fsbuild/PACKAGE`
 VERSION=`cat fsbuild/VERSION`
 if [ "`uname`" = "Linux" ]; then
 OS=Linux
+SYSTEM=Linux
 ARCH=x86_64
 elif [ "`uname`" = "Darwin" ]; then
+SYSTEM=macOS
 OS=macOS
 ARCH=x86_64
 elif [ "`uname -o`" = "Msys" ]; then
+SYSTEM=Windows
 OS=Windows
 ARCH=x86_64
 fi
